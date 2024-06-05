34344cacdfd6a1bbc7dd141d2dffdf661ccfa83e
kiril <kkornienko885@gmail.com>
2024-06-05 12:54:36 +0300

    back

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import Topic
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 def index(request):
     """Головна сторінка "Журналу спостережень"."""
@@ -15,7 +16,48 @@ def topics(request):
 def topic(request, topic_id):
     """Показувати одну тему та всі записи в ній"""
     topic = Topic.objects.get(id=topic_id)
-    entries = topic.entry_set.order_by('-date_added')
+    entries = topic.entry_set.order_by('-data_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+def new_topic(request):
+    """Додає нову тему"""
+    if request.method != 'POST':
+        # Жодних даних не відправлено створити нову форму
+        form = TopicForm()
+    else:
+        # Відправлений пост, обробити дані
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # Показати порожню або недійсну форму
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Додати новий довис до теми"""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        # Жодних даних не надісланно, створити порожню ФОРМУ
+        form = EntryForm()
+    else:
+        # Отримані дані у ПОСТ-запиті, обробити дані
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    # Показати порожню або недійсну форму
+    context =  {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """редагувати існуючий запис"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
